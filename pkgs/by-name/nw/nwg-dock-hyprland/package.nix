20512e68dc5090c8970f5bcf7ece0397baf01ddf
TheTabbingman <51281790+TheTabbingman@users.noreply.github.com>
2025-11-30 21:55:21 -0600

    Try fixing nwg-dock-hyprland

--- pkgs/by-name/nw/nwg-dock-hyprland/package.nix
+++ pkgs/by-name/nw/nwg-dock-hyprland/package.nix
@@ -7,12 +7,13 @@
   gobject-introspection,
   gtk-layer-shell,
 }:
-buildGoModule {
+buildGoModule rec {
   pname = "nwg-dock-hyprland";
 
   src = fetchFromGitHub {
     owner = "TheTabbingMan";
     repo = "nwg-dock-hyprland";
+    tag = "cf82483";
     hash = "sha256-a+LNABfe5rHStZrf+qlayNuCwTQf+7qY4xjx2K7Ky7E=";
   };
 
